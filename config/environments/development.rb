14016a9f1af5e737d879a1fc67fd57b45a52ea13
vagrant <vagrant@kellogg-dev-box>
2014-05-09 15:32:45 +0000

    First commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Homework::Application.configure do
+=======
+Rails.application.configure do
+>>>>>>> Generate the app
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -19,11 +22,26 @@ Homework::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+<<<<<<< HEAD
   # Raise an error on page load if there are pending migrations
+=======
+  # Raise an error on page load if there are pending migrations.
+>>>>>>> Generate the app
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+<<<<<<< HEAD
+=======
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+>>>>>>> Generate the app
 end
